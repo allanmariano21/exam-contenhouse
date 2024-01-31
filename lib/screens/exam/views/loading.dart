4cdb26f38f4557aba1de43e5e3f6fd62f206e191
Allan Mariano <allan.mariano@mobilemindsinc.com>
2024-01-31 13:22:22 +0800

    no message

--- lib/screens/exam/views/loading.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:mobile_exam/core/app/view.dart' as base;
-
-class ViewState extends base.ViewState {
-  @override
-  Widget content(BuildContext context) {
-    return const Center(child: CircularProgressIndicator());
-  }
-}
